bd07e78afffb598dc7bdcb6e5e0fdb2ed066621c
HetviS7Span <hetvi@7span.com>
2023-01-17 16:29:38 +0530

    initial commit

---
+++ src/main/java/com/example/javaGenerics/JavaGenericsApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaGenerics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGenericsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGenericsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaGenerics/JavagenericsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaGenerics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavagenericsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
